deb965eb980d751109b8b6fc39679083fe00f31a
amirzarafshani@gmail.com <amirzarafshani@gmail.com>
2023-06-29 22:25:13 +0300

    referral isue

--- app/config.js
+++ app/config.js
@@ -1,6 +1,4 @@
 export default {
   rootUrl: 'https://api.diamond-stake.com/',
   apiUrl: 'https://api.diamond-stake.com/admin/',
-  rootUrl: 'http://192.168.1.53:3003/',
-  apiUrl: 'http://192.168.1.53:3003/admin/',
 };
